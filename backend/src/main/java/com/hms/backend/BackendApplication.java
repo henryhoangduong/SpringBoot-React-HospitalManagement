3bd5368e142337c3ac723fa779c2898c19020dd4
henryhoangduong <henryhoangduong@gmail.com>
2025-03-03 21:27:04 +0700

    configure dotenv

--- backend/src/main/java/com/hms/backend/BackendApplication.java
+++ backend/src/main/java/com/hms/backend/BackendApplication.java
@@ -1,12 +1,16 @@
 package com.hms.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
