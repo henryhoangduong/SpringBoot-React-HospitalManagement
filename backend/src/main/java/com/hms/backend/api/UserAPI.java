55b61a8db57fe35268510a9066dcfe8c0da99cd0
henryhoangduong <henryhoangduong@gmail.com>
2025-03-06 00:18:08 +0700

    register user

--- backend/src/main/java/com/hms/backend/api/UserAPI.java
+++ backend/src/main/java/com/hms/backend/api/UserAPI.java
@@ -1,14 +1,26 @@
 package com.hms.backend.api;
 
+import com.hms.backend.dto.ResponseDTO;
+import com.hms.backend.dto.UserDTO;
+import com.hms.backend.exception.HMSException;
+import com.hms.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 @Validated
 @CrossOrigin
 public class UserAPI {
+    @Autowired
+    private UserService userService;
 
+    @PostMapping("/register")
+    public ResponseEntity<ResponseDTO> registerUser(@RequestBody  UserDTO userDTO)throws HMSException {
+        userService.registerUser(userDTO);
+        return new ResponseEntity<>(new ResponseDTO("user created"), HttpStatus.CREATED);
+    }
 }
